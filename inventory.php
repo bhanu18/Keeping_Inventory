d6a953421653ba59e75b0fc84402c3d5b876a7ae
Bhanu Mansinghani <bhanuvidh@windowslive.com>
2020-10-09 15:28:37 +0700

    First batch

---
+++ inventory.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+      <?php include_once('header.php')?>
+          <div class="container">
+              <div class="jumbotron">
+                  <h1 class="display-4">Inventory</h1>
+                  <a class="btn btn-primary" href="add_product.php" role="button">Add Product</a>
+                  <a class="btn btn-primary" href="add_inventory.php" role="button">Add Inventory</a>
+          <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Color</th>
+      <th scope="col">Quantity</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php $link= mysqli_connect('localhost','root','','store','3308');
+if(!$link){
+    die ('connection unsuccessful'. mysqli_error($link));
+}
+      $sql = "SELECT Name,price,Color, Quantity FROM product JOIN inventory on inventory.Product_id= product.Id";
+      
+      $result = mysqli_query($link,$sql);
+      
+      while($row = mysqli_fetch_array($result)){
+          echo '<tr>
+      <td>'.$row["Name"].'</td>
+      <td>'.$row["price"].'</td>
+      <td>'.$row["Color"].'</td>
+      <td>'.$row["Quantity"].'</td>
+    </tr>';
+      }
+      mysqli_close($link);
+      ?>
+  </tbody>
+</table>
+      </div>
+</div>
+    
+  </body>
+</html>
