e531fd759180cfe5c200d9c41b44c9f8e09ef248
Bhanuvidh Mansinghani <bhanuvidh@windowslive.com>
2021-04-03 21:38:06 +0700

    added sign function

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
+include_once("create_user.php");
 include_once("login.php");
 if(isset($_SESSION['login_user'])){
 header("location: home.php");
  }
-?><!doctype html>
+?>
+<!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
       <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <!-- Optional JavaScript -->
@@ -24,59 +32,127 @@ header("location: home.php");
 }
 .toggleForms {
 font-weight: bold;
+}
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-align: center;
+  -ms-flex-pack: center;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
 }</style>
   </head>
-  <body>
+  <body class="text-center">
 
-    <div class="container" id="signUpForm">
-              <div class="jumbotron">
-<form method="post" action="create_user.php">
-    <h1>Sign Up!</h1>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign Up</button>
-    <p><a class="toggleForms">Log in</a></p>
-</form>
-          </div>
+  <form method="post" class="form-signin" id="signUpForm" name="signUpForm" onsubmit="return FormValidation()">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign Up</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <div id="validate_email"></div>
+      <input type="email" id="inputEmail" name="username" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <div id="validate_password"></div>
+      <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+      <label for="inputPassword" class="sr-only">Comfirm Password</label>
+      <div id="validate_confirm_password"></div>
+      <input type="password" id="inputPassword" name="comfirm_password" class="form-control" placeholder="Comfirm Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
       </div>
-       <div class="container" id="logInForm">
-              <div class="jumbotron">
-                  <form method="post">
-                      <h1>Sign In!</h1>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" name="username"id="email" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" name="password" id="password">
-  </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
+      <p><a class="toggleForms">Sign In</a></p>
+      <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
+    </form>
 
-  <button type="submit" class="btn btn-primary">Sign In</button>
-                      <span><?php echo $error; ?></span>
-                      <p><a class="toggleForms">Sign Up</a></p>
+      <form method="post"class="form-signin" id="logInForm">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" name="username" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <!-- <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label> -->
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
+      <p><a class="toggleForms">Sign Up</a></p>
+      <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
+      <span><?php echo $error; ?></span>
+    </form>
 </form>
            </div>
       </div>
 <script type="text/javascript">
       
         $(".toggleForms").click(function() {
-            
             $("#signUpForm").toggle();
             $("#logInForm").toggle();
         });
+
+        function FormValidation(){
+          var email = documemt.forms["signUpForm"]["username"].value;
+          var password = document.forms["signUpForm"]["password"].value;
+          var comfirm_password = document.forms["signUpForm"]["confirm_password"].value;
+
+          if (email == ''){
+            document.getElementbyId('validate_email').innerHTML="Require Email";
+            return false;
+          }
+          if(password == ''){
+            document.getElementbyId('validate_password').innerHTML = "Require Password";
+            return false;
+          }
+          if(confirm_password == ''){
+            document.getElementId('validate_confirm_password').innerHTML = "Please Confirm Password";
+            return false;
+          }
+
+        }
       </script>
       
     
